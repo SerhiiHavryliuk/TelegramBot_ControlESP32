6cb4b4eb6ce4411c74ef83184a63ae8cbcef9776
Serhii H Inv <serhii.havryliuk@eliftech.com>
2025-04-06 18:52:02 +0300

    add access for multiple users

--- README.md
+++ README.md
@@ -2,13 +2,13 @@
 This code shows how to control the ESP32 GPIOs from anywhere in the world using Telegram.
 
 Esp32 ttgo LilyGo T-Dispaly
-Do not use 2, 12 pins for rele/
+Do not use 2, 12 pins for rele
 
 05.04.2025 (PolygonAjaxRebootHubs_bot branch)
   Serhii - copy project https://RandomNerdTutorials.com/telegram-control-esp32-esp8266-nodemcu-outputs/
 
 ### Telegram commands:
-  /start returns all command
+  /start returns to returns all command
 
 ### Message:
 
@@ -31,3 +31,17 @@ Use the following commands to control your outputs.
 /reboot_hub_8 to reboot Hub 8
 
 /reboot_all to reboot all Hubs
+
+
+
+### Помилка Unauthorized user виникає ось тут:
+
+  String chat_id = String(bot.messages[i].chat_id);
+  if (chat_id != CHAT_ID){
+    bot.sendMessage(chat_id, "Unauthorized user", "");
+    continue;
+  }
+
+Треба прописати ід всіх юзерів, кому є доступ!
+todo: хотів додавати бота у спільний чаті з чату керувати ботом. Поки не вийшло! 
+Бот висне, після відправки одного повідомлення. Допомогло тільки видалення та створення бота заново.
--- TelegramBot_ControlESP32.ino
+++ TelegramBot_ControlESP32.ino
@@ -9,12 +9,12 @@
 */
 
 #ifdef ESP32
-  #include <WiFi.h>
+#include <WiFi.h>
 #else
-  #include <ESP8266WiFi.h>
+#include <ESP8266WiFi.h>
 #endif
 #include <WiFiClientSecure.h>
-#include <UniversalTelegramBot.h>   // Universal Telegram Bot Library written by Brian Lough: https://github.com/witnessmenow/Universal-Arduino-Telegram-Bot
+#include <UniversalTelegramBot.h>  // Universal Telegram Bot Library written by Brian Lough: https://github.com/witnessmenow/Universal-Arduino-Telegram-Bot
 #include <ArduinoJson.h>
 
 // Serhii Credentials ------------------------------------------------------------------------------------------------------
@@ -28,11 +28,21 @@ const char* password = "REPLACE_WITH_YOUR_PASSWORD";
 // Use @myidbot to find out the chat ID of an individual or a group
 // Also note that you need to click "start" on a bot before it can
 // message you
-#define CHAT_ID "XXXXXXXXXX"
+// List of people who has access to bot
+#define ID_User_1 "xxxxxxxxx"
+#define ID_User_2 "xxxxxxxxx"
+#define ID_User_3 "xxxxxxxxx"
+// Team id
+#define ID_User_3 "xxxxxxxxx"
+#define ID_User_4 "xxxxxxxxx"
+#define ID_User_5 "xxxxxxxxx"
+#define ID_User_6 "xxxxxxxxx"
+#define ID_User_7 "xxxxxxxxx"
+#define ID_User_8 "xxxxxxxxx"
 // Serhii Credentials ------------------------------------------------------------------------------------------------------
 
 #ifdef ESP8266
-  X509List cert(TELEGRAM_CERTIFICATE_ROOT);
+X509List cert(TELEGRAM_CERTIFICATE_ROOT);
 #endif
 
 WiFiClientSecure client;
@@ -51,21 +61,27 @@ const int hubPin_6 = 17;
 const int hubPin_7 = 22;
 const int hubPin_8 = 21;
 
-// bool hubState_1 = HIGH; // за замовчанням реле викл
-
 // Handle what happens when you receive new messages
 void handleNewMessages(int numNewMessages) {
   Serial.println("handleNewMessages");
   Serial.println(String(numNewMessages));
 
-  for (int i=0; i<numNewMessages; i++) {
+  for (int i = 0; i < numNewMessages; i++) {
     // Chat id of the requester
     String chat_id = String(bot.messages[i].chat_id);
-    if (chat_id != CHAT_ID){
-      bot.sendMessage(chat_id, "Unauthorized user", "");
+    if (chat_id != ID_User_1 
+      && chat_id != ID_User_2 
+      && chat_id != ID_User_3 
+      && chat_id != ID_User_4
+      && chat_id != ID_User_5
+      && chat_id != ID_User_6 
+      && chat_id != ID_User_7 
+      && chat_id != ID_User_8 
+      && chat_id != ID_User_9 ) {
+      bot.sendMessage(chat_id, "Unauthorized user. For access, please contact Serhii_H (Intrusion team)!", "");
       continue;
     }
-    
+
     // Print the received message
     String text = bot.messages[i].text;
     Serial.println(text);
@@ -110,7 +126,7 @@ void handleNewMessages(int numNewMessages) {
     }
     if (text == "/reboot_hub_8") {
       rebootHub(8, hubPin_8, chat_id);
-    } 
+    }
     if (text == "/reboot_all") {
       rebootHub(1, hubPin_1, chat_id);
       rebootHub(2, hubPin_2, chat_id);
@@ -120,11 +136,11 @@ void handleNewMessages(int numNewMessages) {
       rebootHub(6, hubPin_6, chat_id);
       rebootHub(7, hubPin_7, chat_id);
       rebootHub(8, hubPin_8, chat_id);
-    } 
+    }
   }
 }
 
-void rebootHub(int hubNumber, int hubPin, String chat_ID){
+void rebootHub(int hubNumber, int hubPin, String chat_ID) {
   String telegramMessage = "Kurwa bobr! Hub " + String(hubNumber) + " start reboot ... 😏";
   bot.sendMessage(chat_ID, telegramMessage, "");
 
@@ -148,7 +164,7 @@ void rebootHub(int hubNumber, int hubPin, String chat_ID){
 
 // Реле управляється GND, тобто щоб спрацювало реле треба подати мінус
 // тому за замовчанням подаємо + (HIGH), щоб реле не мпрацьовувало
-void initPinRele(int hubPin){
+void initPinRele(int hubPin) {
   pinMode(hubPin, OUTPUT);
   digitalWrite(hubPin, HIGH);
 }
@@ -158,10 +174,10 @@ void setup() {
   Serial.println("Serhii:  device ver 2025.0");
   Serial.println("Serhii: for start bot use /start - return all comands in telegram");
 
-  #ifdef ESP8266
-    configTime(0, 0, "pool.ntp.org");      // get UTC time via NTP
-    client.setTrustAnchors(&cert); // Add root certificate for api.telegram.org
-  #endif
+#ifdef ESP8266
+  configTime(0, 0, "pool.ntp.org");  // get UTC time via NTP
+  client.setTrustAnchors(&cert);     // Add root certificate for api.telegram.org
+#endif
 
   initPinRele(hubPin_1);
   initPinRele(hubPin_2);
@@ -175,9 +191,9 @@ void setup() {
   // Connect to Wi-Fi
   WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
-  #ifdef ESP32
-    client.setCACert(TELEGRAM_CERTIFICATE_ROOT); // Add root certificate for api.telegram.org
-  #endif
+#ifdef ESP32
+  client.setCACert(TELEGRAM_CERTIFICATE_ROOT);  // Add root certificate for api.telegram.org
+#endif
   while (WiFi.status() != WL_CONNECTED) {
     delay(1000);
     Serial.println("Connecting to WiFi..");
@@ -187,10 +203,10 @@ void setup() {
 }
 
 void loop() {
-  if (millis() > lastTimeBotRan + botRequestDelay)  {
+  if (millis() > lastTimeBotRan + botRequestDelay) {
     int numNewMessages = bot.getUpdates(bot.last_message_received + 1);
 
-    while(numNewMessages) {
+    while (numNewMessages) {
       Serial.println("got response");
       handleNewMessages(numNewMessages);
       numNewMessages = bot.getUpdates(bot.last_message_received + 1);
